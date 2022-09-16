dfc32e3c9c5c8bf8df1cc4ed838863bc3b6f982a
GVSS Tarun <113722788+Gvsstarun@users.noreply.github.com>
2022-09-17 02:00:17 +0530

    Update streamlit_app.py
    spelling mistake for title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.titile('Breakfast Menu /n Omega 3 & Blueberry Oatmeal /n kale, spinach and Rocket smoothie /n Hard-boiled- Free-range Egg')
+streamlit.title('Breakfast Menu /n Omega 3 & Blueberry Oatmeal /n kale, spinach and Rocket smoothie /n Hard-boiled- Free-range Egg')
