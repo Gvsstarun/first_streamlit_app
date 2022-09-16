991bfc42d64d4a5aeb6797b7dbd9c02da53643b7
GVSS Tarun <113722788+Gvsstarun@users.noreply.github.com>
2022-09-17 02:03:46 +0530

    Update streamlit_app.py
    made same as example

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal') 
-streamlit.title('kale, spinach and Rocket smoothie')
-streamlit.title('Hard-boiled- Free-range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal') 
+streamlit.text('kale, spinach and Rocket smoothie')
+streamlit.text('Hard-boiled- Free-range Egg')
