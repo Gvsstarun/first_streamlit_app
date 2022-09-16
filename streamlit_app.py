4d94f4a960a70bd77a3de8d22cfa44777e4144a2
GVSS Tarun <113722788+Gvsstarun@users.noreply.github.com>
2022-09-17 01:59:57 +0530

    Update streamlit_app.py
    changed to title from body

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.body('Breakfast Menu /n Omega 3 & Blueberry Oatmeal /n kale, spinach and Rocket smoothie /n Hard-boiled- Free-range Egg')
+streamlit.titile('Breakfast Menu /n Omega 3 & Blueberry Oatmeal /n kale, spinach and Rocket smoothie /n Hard-boiled- Free-range Egg')
